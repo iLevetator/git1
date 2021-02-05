badea4baccde694a4cd4dd515256fa3e577cefd2
Levetator <ilevetator@yandex.ru>
2021-02-05 14:50:59 +0300

    second version upd

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import sys
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5 import uic
+from UI import Ui_Form
 from random import randint
 
 
-class GoodMood(QWidget):
+class GoodMood(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.btn.clicked.connect(self.draw)
         self.drawing = False
 
     def paintEvent(self, event):
         if self.drawing:
             qp = QPainter(self)
-            qp.setPen(QColor(255, 255, 0))
+            qp.setPen(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
             r = randint(0, min(self.window().width(), self.window().height())) // 2
             qp.drawEllipse(randint(0, self.window().width() - 2 * r),
                            randint(0, self.window().height() - 2 * r), r, r)
